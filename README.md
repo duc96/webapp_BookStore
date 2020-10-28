03f50be8fc1fb8dece36999f33c12148da3cedd3
CauHuy <cauhuyso000@gmail.com>
2020-10-28 11:38:25 +0700

    webAdmin

--- README.md
+++
@@ -1,3 +0,0 @@
-# webapp_BookStore
-Ae clone về nhớ NPM INSTALL  trước khi npm start nhé !!!
-good luck to you !!!
--- public/js/caculator.js
+++
@@ -1,9 +0,0 @@
-exports.cong = function(a,b){
-    return a+b;
-};
-exports.tru = function(a,b){
-    return a-b;
-};
-exports.nhan = function(a,b){
-    return a*b;
-};
